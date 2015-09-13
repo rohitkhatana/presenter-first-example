d3ad8396ef1cbdc2668a8f1fc90438b30c0e4f59
Rohit Khatana <rohitkhatana.khatana@gmail.com>
2015-09-13 07:24:52 +0530

    done, but remaing with holding the this reference

--- javascript/built/out.js
+++ javascript/built/out.js
@@ -2,19 +2,74 @@ var UserRegistration;
 (function (UserRegistration) {
     var View = (function () {
         function View() {
+            this.announcer = riot.observable(this);
+            this.registerWithForm();
         }
+        View.prototype.whenUserRegister = function (callback) {
+            console.log("user is registered");
+            this.announcer.on("whenUserRegister", callback);
+        };
+        View.prototype.showWelcomeMessage = function () {
+            console.log("welcome user");
+        };
+        View.prototype.showFullName = function () {
+            this.fullName = $("#first-name").val() + " " + $("#last-name").val();
+            $("#full-name").html(this.fullName);
+        };
+        View.prototype.registerWithForm = function () {
+            var _this = this;
+            $("#user-form").submit(function (e) {
+                e.preventDefault();
+                _this.showFullName();
+                _this.announcer.trigger("whenUserRegister", function () { fullName: _this.fullName; });
+            });
+        };
         return View;
     })();
     UserRegistration.View = View;
     var Presenter = (function () {
         function Presenter(view, model) {
+            this.view = view;
+            this.model = model;
+            view.whenUserRegister(this.saveUserCallback);
+            model.whenUserIsSaved(this.showWelcomeMessage);
         }
+        Presenter.prototype.self = function () {
+            return this;
+        };
+        Presenter.prototype.saveUserCallback = function (userData) {
+            //call the model the same way
+            console.log(this);
+            this.self().model.saveUser(userData);
+            console.log("data is transferred");
+        };
+        Presenter.prototype.showWelcomeMessage = function () {
+            this.view.showWelcomeMessage();
+            //call view to render welcome message;
+        };
         return Presenter;
     })();
     UserRegistration.Presenter = Presenter;
     var Model = (function () {
         function Model() {
+            this.announcer = riot.observable(this);
         }
+        Model.prototype.saveUser = function (userData) {
+            var _this = this;
+            $.ajax({
+                type: 'POST',
+                url: $(this).attr("action"),
+                data: JSON.stringify(userData),
+                success: function (data) {
+                    _this.announcer.trigger("whenUserIsSaved");
+                },
+                contentType: "application/json",
+                dataType: 'json'
+            });
+        };
+        Model.prototype.whenUserIsSaved = function (callback) {
+            this.announcer.on("whenUserIsSaved", callback);
+        };
         return Model;
     })();
     UserRegistration.Model = Model;
--- javascript/user_registration.ts
+++ javascript/user_registration.ts
@@ -1,27 +1,94 @@
 module UserRegistration {
 	declare var $: any;
 	declare var riot: any;
-	
-	interface SerializeUserInformation {
 
+	interface SerializeUserInformation {
+		whenUserRegister(callback: (userData: JSON) => void): void;
+		showWelcomeMessage();
 	}
 
 	interface RegisterUser {
-
+		saveUser(userData: JSON): void;
+		whenUserIsSaved(callback: () => void): void;
 	}
 
-	export class View implements SerializeUserInformation { 
+	export class View implements SerializeUserInformation {
+		fullName: string; 
+		announcer = riot.observable(this);
+
+		constructor(){
+			this.registerWithForm();
+		}
+
+		whenUserRegister(callback: (userData: JSON) => void){
+			console.log("user is registered");
+			this.announcer.on("whenUserRegister", callback);
+		}
+
+		showWelcomeMessage(){
+			console.log("welcome user");
+		}
+		
+		private showFullName() {
+			this.fullName = $("#first-name").val() + " " + $("#last-name").val();
+				$("#full-name").html(this.fullName);
+		}
+
+		private registerWithForm(){
+			var _this = this;
+			$("#user-form").submit(function(e) { 
+				e.preventDefault();
+				_this.showFullName();
+				_this.announcer.trigger("whenUserRegister", () => {fullName: _this.fullName});	
+			});
+		}
+
 
 	}
 	
 	export class Presenter {
-		constructor(view: SerializeUserInformation, model: RegisterUser){
+		constructor(private view: SerializeUserInformation, private model: RegisterUser){
+			view.whenUserRegister(this.saveUserCallback);
+			model.whenUserIsSaved(this.showWelcomeMessage);
+		}
 
+		saveUserCallback(userData: JSON){
+			//call the model the same way
+			console.log(this);
+			this.model.saveUser(userData);
+			console.log("data is transferred");
+		}
+
+		showWelcomeMessage(){
+			this.view.showWelcomeMessage();
+			//call view to render welcome message;
 		}
 	} 
 
 
 	export class Model implements RegisterUser {
+		announcer = riot.observable(this);
+		
+		constructor(){
 
+		}
+
+		saveUser(userData: JSON){
+			var _this = this;
+			$.ajax({
+				type: 'POST',
+				url: $(this).attr("action"),
+				data: JSON.stringify(userData),
+				success: function(data) {
+					_this.announcer.trigger("whenUserIsSaved");
+				},
+				contentType: "application/json",
+				dataType: 'json'
+			});
+		}
+
+		whenUserIsSaved(callback: () => void){
+			this.announcer.on("whenUserIsSaved", callback);
+		}
 	}	
 }
